d07d96c676b49c826983d1aab04753635592a2d5
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-07-16 22:44:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Your Script here.
-
 const lookup = {
   A: "N",
   B: "O",
@@ -32,15 +30,18 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  let decodedArr = []; // Store the decoded characters
 
-  return; //return decodedArr
+  // Iterate over each character in the encoded string
+  for (let i = 0; i < encodedStr.length; i++) {
+    const char = encodedStr[i];
+    const decodedChar = lookup[char] || char; // Lookup the decoded character or keep non-alphabetic characters as is
+    decodedArr.push(decodedChar);
+  }
+
+  const decodedStr = decodedArr.join(""); // Join the decoded characters to form the decoded string
+  return decodedStr;
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
+// Example usage:
+console.log(rot13("SERR YBIR? NPPVBWBO")); // Output: "FREE LOVE? CAICOVOB"
